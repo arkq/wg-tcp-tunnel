87229b24b6cdd2158f14a65e6532b0a63c8ebc67
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2023-11-05 22:41:32 +0100

    Initial commit for WireGuard TCP tunneling

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+//
+// Copyright (c) 2023 Arkadiusz Bokowy
+//
+// This file is a part of wg-tcp-tunnel.
+//
+// This project is licensed under the terms of the MIT license.
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
